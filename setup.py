16906c879e1002548deed7a61f0fa9f27e3ce6ea
Benjamin Grandfond <benjaming@theodo.fr>
2012-02-12 16:55:38 +0100

    Try building a working setup.py... fail

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+
+toilet_dir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'toilet'
 
 setup(
     name="toilet",
     version="0.1",
     author="Benjamin Grandfond",
     author_email="benjaming@theodo.fr",
+    maintainer="Benjamin Grandfond",
+    maintainer_email="benjaming@theodo.fr",
+    url="http://github.com/theodo/toilet",
     description="Theodo toilets availability status ubuntu indicator.",
-    #packages=['os', 'gtk', 'appindicator', 'json', 'urllib2'],
-    scripts=['toilet/indicator.py']
+#    package_dir={'': 'toilet'},
+#    package_dir={
+#      toilet_dir: 'indicator',
+#      toilet_dir: 'toilet',
+#      toilet_dir: 'dataloader', 
+#    },
+    py_modules=['toilet.toilet', 'toilet.dataloader', 'toilet.indicator'],
+    scripts=['app-toilet']
 )
